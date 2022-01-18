ba70c58bb78574d3ddbf1f1c2ab28611f204458d
omerozturk18 <omer_ozturk1997@hotmail.com>
2022-01-18 22:09:59 +0300

    Create N11 Graduation Project Application

---
+++ src/test/java/com/omerozturk/N11GraduationProject/N11GraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omerozturk.N11GraduationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N11GraduationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
